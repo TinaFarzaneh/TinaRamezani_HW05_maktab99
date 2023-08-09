2eb27b8c97b2d02d9794227267cbfe5b7d2092df
Tina.Rf <ramezanift73@gmail.com>
2023-08-09 23:54:38 +0330

    question5 arrowFunction

---
+++ TinaRamezani_hw05_maktab99/Q5/script.js
@@ -0,0 +1,28 @@
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+ask(
+  "Do you agree?",
+  function () {
+    alert("You agreed.");
+  },
+  function () {
+    alert("You canceled the execution.");
+  }
+);
+
+//Arrow function
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+ask(
+  "Do you agree?",
+  () => {
+    alert("You agreed.");
+  },
+  () => {
+    alert("You canceled the execution.");
+  }
+);
